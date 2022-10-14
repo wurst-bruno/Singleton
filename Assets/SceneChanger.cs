9aef95ea5c2de41763ac64cd742dae0db9c6c8ee
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-10-14 10:21:53 -0300

    10:21

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string[] nombresEscenas;
+    public int indexEscenas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CambiarDeEscenaSecuencialmente()
+    {
+        if (indexEscenas >= nombresEscenas.Length)
+        {
+            indexEscenas = 0;
+        }
+
+        SceneManager.LoadScene(nombresEscenas[indexEscenas]);
+        indexEscenas++;
+
+
+
+    }
+}
